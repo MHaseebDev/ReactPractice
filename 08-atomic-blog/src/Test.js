2742deae50970842980f55e18e874f22d1d92faf
Haseeb <haseeb.mahmood@folium.ai>
2024-09-06 14:20:00 +0500

    Peformance optimizations

--- 08-atomic-blog/src/Test.js
+++ 08-atomic-blog/src/Test.js
@@ -1,26 +1,52 @@
-import { useState } from "react";
+import { memo, useMemo, useState } from "react";
 
-function SlowComponent() {
+const SlowComponent = memo(function SlowComponent({ dataObj }) {
   // If this is too slow on your maching, reduce the `length`
   const words = Array.from({ length: 100_000 }, () => "WORD");
   return (
     <ul>
       {words.map((word, i) => (
         <li key={i}>
-          {i}: {word}
+          {i}: {word} : {dataObj.title}
         </li>
       ))}
     </ul>
   );
-}
+});
+// function Counter({ children }) {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       <h1>Slow counter?!?</h1>
+//       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+//       {children}
+//     </div>
+//   );
+// }
+// export default function Test() {
+//   return (
+//     <div>
+//       <Counter>
+//         <SlowComponent />
+//       </Counter>
+//     </div>
+//   );
+// }
 
 export default function Test() {
   const [count, setCount] = useState(0);
+  const dataObj = useMemo(() => {
+    return {
+      title: "Slow",
+      description: "Component",
+    };
+  }, []);
+
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      <SlowComponent dataObj={dataObj} />
     </div>
   );
 }
